00582caa9118741228dafb5e6952fa7426db47e5
tiantianzhu19@gmail.com <tiantianzhu19@gmail.com>
2021-02-28 23:05:36 +0100

    added some code in Value of Information

--- examples/Value of Information.py
+++ examples/Value of Information.py
@@ -16,12 +16,12 @@ def action_success_rate_log(action, t):
     if action == 2:
         p_action = 1
     else:
-        if t >= 9:
+        if t >= 11:
             p_action = 1
         elif t < 4:
             p_action = 0
         else:
-            p_action = log(t - 3, 6)
+            p_action = log(t - 3, 9)
     return p_action
 
 
@@ -109,11 +109,14 @@ def my_range(start, end, step):
         yield start
         start += step
 
-
+def time_range(start, end, step):
+    while start <= end:
+        yield start
+        start += step
 def get_optimal_time_continuous(action):
     max_V = voi(p_accident, C_accident, a_tpr, b_tpr, a_fpr, b_fpr, C_noaccident, 12, action)[3]
 
-    for i in my_range(1, 12, 0.1):
+    for i in time_range(1, 12, 0.1):
         VoI_t = voi(p_accident, C_accident, a_tpr, b_tpr, a_fpr, b_fpr, C_noaccident, i, action)
         # print(i)
         # print(action_success_rate_normal(1,i))
@@ -132,7 +135,7 @@ def sensitivity_study_accident_cost():
     optimal_value_list = []
     optimal_action_list = []
 
-    for k in my_range(-461, -1, 1):
+    for k in my_range(-37, -3, 1):
         max_V = [None] * 2
         VoI_precautionary = [None] * 2
         time = [None] * 2
@@ -145,7 +148,7 @@ def sensitivity_study_accident_cost():
             action = j
             max_V[j - 1] = voi(p_accident, C_accident, a_tpr, b_tpr, a_fpr, b_fpr, C_noaccident, 12, action)[3]
 
-            for i in my_range(1, 12, 0.1):
+            for i in time_range(1, 12, 0.1):
                 VoI_t = voi(p_accident, C_accident, a_tpr, b_tpr, a_fpr, b_fpr, C_noaccident, i, action)
                 # print(i)
                 # print(action_success_rate_normal(1,i))
@@ -176,12 +179,12 @@ def sensitivity_study_accident_cost():
     return optimal_time_list, optimal_action_list, optimal_value_list
 
 
-# print(sensitivity_study_accident_cost())
+print(sensitivity_study_accident_cost())
 def sensitivity_study_accident_probability():
     optimal_time_list = []
     optimal_value_list = []
     optimal_action_list = []
-    C_accident = -600
+    C_accident = -30
     a_tpr = 0.5
     b_tpr = 0.48
     a_fpr = 0.1556
@@ -191,7 +194,7 @@ def sensitivity_study_accident_probability():
     sigma = 1
 
 
-    for k in my_range(0, 1, 0.01):
+    for k in my_range(0, 0.5, 0.01):
         max_V = [None] * 2
         VoI_precautionary = [None] * 2
         time = [None] * 2
@@ -204,7 +207,7 @@ def sensitivity_study_accident_probability():
             action = j
             max_V[j - 1] = voi(p_accident, C_accident, a_tpr, b_tpr, a_fpr, b_fpr, C_noaccident, 12, action)[3]
 
-            for i in my_range(1, 12, 0.1):
+            for i in time_range(1, 12, 0.1):
                 VoI_t = voi(p_accident, C_accident, a_tpr, b_tpr, a_fpr, b_fpr, C_noaccident, i, action)
                 # print(i)
                 # print(action_success_rate_normal(1,i))
@@ -234,7 +237,7 @@ def sensitivity_study_accident_probability():
     return optimal_time_list, optimal_action_list, optimal_value_list
 
 
-#print(sensitivity_study_accident_probability())
+print(sensitivity_study_accident_probability())
 
 
 def sensitivity_study_success_rate():
@@ -264,7 +267,7 @@ def sensitivity_study_success_rate():
             action = j
             max_V[j - 1] = C_accident
 
-            for i in my_range(1, 12, 0.1):
+            for i in time_range(1, 12, 0.1):
                 TPR = tpr(i, a_tpr, b_tpr)
                 FPR = fpr(i, a_fpr, b_fpr)
                 p_action = action_success_rate_normal(j, i, mu, sigma)
@@ -302,7 +305,6 @@ def sensitivity_study_cost_ratio():
     optimal_time_list = []
     optimal_value_list = []
     optimal_action_list = []
-    C_accident = -28
     a_tpr = 0.5
     b_tpr = 0.48
     a_fpr = 0.1556
@@ -311,15 +313,16 @@ def sensitivity_study_cost_ratio():
     p_accident = 0.1
     mu = 6
     sigma = 2
-    for k in my_range(0.01, 1, 0.01):
+    C_action=-2
+    for k in my_range(1, 50, 0.5):
 
         list = []
-        C_action = C_accident*k
+        C_accident = C_action*k
 
         VoI_neglecting_risk = (1 - p_accident) * C_noaccident + p_accident * C_accident
         max_V = C_accident
 
-        for i in my_range(1, 12, 0.1):
+        for i in time_range(1, 12, 0.1):
             TPR = tpr(i, a_tpr, b_tpr)
             FPR = fpr(i, a_fpr, b_fpr)
             p_action = action_success_rate_normal(1, i, mu, sigma)
@@ -357,4 +360,63 @@ def sensitivity_study_cost_ratio():
 
     return optimal_time_list, optimal_action_list, optimal_value_list
 
-print(sensitivity_study_cost_ratio())
+#print(sensitivity_study_cost_ratio())
+def sensitivity_study():
+    optimal_time_list = []
+    optimal_value_list = []
+    optimal_action_list = []
+    C_accident = -28
+    a_tpr = 0.3
+    b_tpr = 0.68
+    a_fpr = 0.1556
+    b_fpr = -0.1445
+    C_noaccident = 0
+    p_accident = 0.1
+    mu = 5
+    sigma = 2
+    C_action = -2
+    for k in my_range(0.01, 1, 0.01):
+
+        list = []
+        VoI_neglecting_risk = (1 - p_accident) * C_noaccident + p_accident * C_accident
+        max_V = C_accident
+        C_action=C_accident*k
+
+        for i in time_range(1, 12, 0.1):
+            TPR = a_tpr + b_tpr * log(13 - i, 12)
+            FPR = a_fpr + b_fpr * log(13 - i, 12)
+            p_action = action_success_rate_normal(1, i, mu, sigma)
+            VoI_precautionary = (1 - p_accident) * C_noaccident + C_action + (1 - p_action) * C_accident
+            VoI_neglecting_risk = (1 - p_accident) * C_noaccident + p_accident * C_accident
+            max_VoI = max(VoI_precautionary, VoI_neglecting_risk)
+            V = ((1 - p_accident) * C_noaccident) + (p_accident * C_accident) + (
+                    (1 - p_accident) * FPR * (C_action - C_noaccident)) + (
+                        p_accident * TPR * (C_action - p_action * C_accident))
+            VoP = V - max_VoI
+            # print(i)
+            # print(action_success_rate_normal(1,i))
+            if V >= VoI_neglecting_risk:
+                max_V = V
+                action_time = i
+        VoI_precautionary = C_action
+        list.append(max_V)
+        list.append(VoI_precautionary)
+        list.append(VoI_neglecting_risk)
+        optimal_value = max(list)
+        max_index = list.index(optimal_value)
+        if max_index <= 0:
+            optimal_action = max_index + 1
+            optimal_time = action_time
+        elif max_index == 2:
+            optimal_action = "neglecting risk "
+            optimal_time = 0
+        else:
+            optimal_action = "being precautionary and take action immediately"
+            optimal_time = 12
+
+        optimal_time_list.append(optimal_time)
+        optimal_value_list.append(optimal_value)
+        optimal_action_list.append(optimal_action)
+
+    return optimal_time_list, optimal_action_list, optimal_value_list
+print(sensitivity_study())
